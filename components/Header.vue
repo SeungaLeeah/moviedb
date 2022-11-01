f45b60f5420181cb9c28e20cfdfa1226dd29807d
SeungaLeeah <92766716+SeungaLeeah@users.noreply.github.com>
2022-11-01 22:23:37 +0900

    moviedb

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img class="mainLogo" src="../assets/imgs/mainlogo.png" alt="mainlogo"/>
+    <div class="text-container">
+        <ul>
+            <li>영화</li>
+            <li>TV프로그램</li>
+            <li>인물</li>
+            <li>More</li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names, vue/no-reserved-component-names
+    name: 'Header',
+}
+</script>
+
+<style scoped>
+  .mainLogo{
+    height: 100px;
+    width: 100px;
+  }
+</style>
