d0ebdc75769d553a24ffb95488295bae08e7ec94
SeungaLeeah <92766716+SeungaLeeah@users.noreply.github.com>
2022-11-01 23:14:11 +0900

    moviedb

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Header />
+  <div></div>
 </template>
 
 <script>
+import axios from '@nuxtjs/axios';
 export default {
-  name: 'IndexPage',
+  data() {
+    return {
+      moives: [],
+    }
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=dca826d7ecad36cf56d8b27692dac7ac&language=en-US&page=1`
+      )
+      const result = await data
+        console.log(result) 
+    },
+  },
 }
 </script>
